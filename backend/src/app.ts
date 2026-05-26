2972f62875148942775eb6704664a562cd9cd751
Nícolas Alves Nascimento <gabi123650@gmail.com>
2026-05-26 15:41:01 -0300

    .

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import Express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import api from "./app/routes/api.ts";
+import path from "path";
 
 const app = Express();
 
@@ -11,14 +12,21 @@ const app = Express();
 
 app.use(cookieParser());
 app.use(Express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5500", "http://127.0.0.1:5500"],
+    credentials: true,
+  }),
+);
 
 // ===========================================================================
 // Rotas do backend
 // ===========================================================================
 
 app.use("/api", api);
+app.use("/", Express.static(path.resolve("../../frontend/")))
+
 
 app.listen(8080, () => {
-    console.log("http://localhost:8080");
+  console.log("http://localhost:8080");
 });
