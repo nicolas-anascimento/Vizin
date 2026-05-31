a98776f6e264075981dd923f6ad2a8dda7e4e666
Nicolas Alves Nascimento <gabi123650@gmail.com>
2026-05-31 19:12:38 -0300

    sincronizando com a main

---
+++ frontend/js/resetar-senha.js
@@ -0,0 +1,61 @@
+
+document.getElementById("formResetar")
+.addEventListener("submit", async (e)=>{
+
+    e.preventDefault();
+
+    const senha =
+    document.getElementById("novaSenha").value;
+
+    const confirmar =
+    document.getElementById("confirmarSenha").value;
+
+    const mensagem =
+    document.getElementById("mensagem");
+
+    if(senha !== confirmar){
+
+        mensagem.innerHTML =
+        "❌ As senhas não coincidem.";
+
+        return;
+    }
+
+    try{
+
+        /*
+        BACK-END FUTURO
+ */
+        const token =
+        new URLSearchParams(window.location.search)
+        .get("token");
+
+        await fetch("/api/contas/resetar-senha",{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({
+                token,
+                senha
+            })
+        });
+       
+
+        mensagem.innerHTML =
+        "✅ Senha alterada com sucesso!";
+
+        setTimeout(()=>{
+
+            window.location.href="/login";
+
+        },2000);
+
+    }catch{
+
+        mensagem.innerHTML =
+        "❌ Erro ao alterar senha.";
+
+    }
+
+});
