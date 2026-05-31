4413dc53a1667d818cdb5709aed4c6618ae71b53
Joélicy Arruda <arrudajoelicy@gmail.com>
2026-05-31 18:14:55 -0300

    Add files via upload

---
+++ frontend/js/recuperar-senha.js
@@ -0,0 +1,36 @@
+document.getElementById("formRecuperar")
+.addEventListener("submit", async (e)=>{
+
+    e.preventDefault();
+
+    const email =
+    document.getElementById("email").value;
+
+    const mensagem =
+    document.getElementById("mensagem");
+
+    try{
+
+        /*
+        BACK-END FUTURO:
+
+        await fetch("/api/recuperar-senha",{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({email})
+        });
+        */
+
+        mensagem.innerHTML =
+        "✅ Se existir uma conta com este email, você receberá instruções para redefinir sua senha.";
+
+    }catch{
+
+        mensagem.innerHTML =
+        "❌ Erro ao solicitar recuperação.";
+
+    }
+
+});
