9437c1b2222f10986df44fd9e0ccd553cd6d0f27
Nícolas Alves Nascimento <gabi123650@gmail.com>
2026-07-30 14:57:44 -0300

    .

--- backend/src/app/routes/web.ts
+++ backend/src/app/routes/web.ts
@@ -7,44 +7,72 @@ import { createHash } from "crypto";
 const web = Express();
 
 function view(view: String) {
-    return path.resolve(`../frontend/${view}`);
+  return path.resolve(`../frontend/${view}/index.html`);
 }
 
 web.get("/", (_req, res) => {
-    res.sendFile(view("login.html"));
+  res.sendFile(view("login"));
 });
 web.get("/login", (_req, res) => {
-    res.sendFile(view("login.html"));
+  res.sendFile(view("login"));
 });
 web.get("/home", (_req, res) => {
-    res.sendFile(view("home.html"));
+  res.sendFile(view("inicio"));
 });
 web.get("/admin", isAdmin, (_req, res) => {
-    res.sendFile(view("dashboard-admin.html"));
+  res.sendFile(view("dashboard-admin"));
 });
 web.get("/objetos", (_req, res) => {
-    res.sendFile(view("meusobjetos.html"));
+  res.sendFile(view("meusobjetos"));
 });
 
 web.get("/recuperar-senha", (_req, res) => {
-    res.sendFile(view("recuperar-senha.html"));
+  res.sendFile(view("recuperar-senha"));
 });
 
 web.get("/resetar-senha", async (req, res) => {
-    const token = req.query.token as string;
+  const token = req.query.token as string;
 
-    const resetRequest = await prisma.resetar_Senha.findUnique({
-        where: {
-            token: createHash("sha256").update(token).digest("hex"),
-        },
-    });
+  const resetRequest = await prisma.resetar_Senha.findUnique({
+    where: {
+      token: createHash("sha256").update(token).digest("hex"),
+    },
+  });
 
-    if (!resetRequest || resetRequest.expire_in < new Date()) {
-        res.redirect("/?error=invalid-token");
-        return;
-    }
+  if (!resetRequest || resetRequest.expire_in < new Date()) {
+    res.redirect("/?error=invalid-token");
+    return;
+  }
 
-    res.sendFile(view("resetar-senha.html"));
+  res.sendFile(view("resetar-senha"));
+});
+
+web.get("/cadastrar-objeto", (_req, res) => {
+  res.sendFile(view("Cadastrar-objeto"));
+});
+
+web.get("/editar-objeto", (_req, res) => {
+  res.sendFile(view("Editar-objeto"));
+});
+
+web.get("/finalizar-pagamento", (_req, res) => {
+  res.sendFile(view("Finalizar-pagamento"));
+});
+
+web.get("/notificacoes", (_req, res) => {
+  res.sendFile(view("Notificacoes"));
+});
+
+web.get("/perfil", (_req, res) => {
+  res.sendFile(view("Perfil"));
+});
+
+web.get("/produto", (_req, res) => {
+  res.sendFile(view("Produto"));
+});
+
+web.get("/retirada-objeto", (_req, res) => {
+  res.sendFile(view("Retirada-objeto"));
 });
 
 export default web;
